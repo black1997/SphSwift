66316a91737a376a5b9278a4c3379cfc3d0706a9
black1997 <wenjuu@163.com>
2020-03-06 16:04:41 +0800

    单元测试

---
+++ Sph/Common/common.swift
@@ -0,0 +1,13 @@
+//
+//  common.swift
+//  Sph
+//
+//  Created by 青天揽月1 on 2020/3/6.
+//  Copyright © 2020 wenjuu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SCR_W = UIScreen.main.bounds.width
+let SCR_H = UIScreen.main.bounds.height
